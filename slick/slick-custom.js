5af7a6a7b1ff68debdc2574f5460df4e1904c7d4
linojung <46038961+linojung@users.noreply.github.com>
2023-09-05 14:16:06 +0900

    handling slick

---
+++ slick/slick-custom.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+  var $magicSlide = $(".magic__slide");
+  $magicSlide.each(function () {
+    $(".magic__slide").slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 5,
+      adaptiveHeight: false,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      centerMode: true,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 1280,
+          settings: {
+            slidesToShow: 3,
+            infinite: false,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: false,
+          },
+        },
+      ],
+    });
+    var $magicPage = $(".magic__page");
+    $(this).on("afterChange", function (event, slick, currentSlide) {
+      $magicPage.find(".magic__page__current").text(currentSlide + 1);
+    });
+    var magicPageTotal = $(this)
+      .find(".slick-slide")
+      .not(".slick-cloned").length;
+    $magicPage.find(".magic__page__total").text(magicPageTotal);
+  });
+});
